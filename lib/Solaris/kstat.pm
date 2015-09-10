333184cfe5d408715f66a87d09fc55d8ca28bc74
Gordon Marler <gmarler@gmarler.com>
2015-09-10 12:49:07 -0400

    Add POD documentation for gethrtime

--- lib/Solaris/kstat.pm
+++ lib/Solaris/kstat.pm
@@ -39,3 +39,17 @@ Instead, use the deep-copy clone() function from the Clone module to make copies
 of the hashref recursively.
 
 =cut
+
+=head1 UTILITY FUNCTIONS
+
+=head2 gethrtime()
+
+Exposes the native gethrtime() function from Solaris, so we don't have to mess
+with fractional seconds, and can use unsigned 64-bit nanosecond resolution
+counters.
+
+Returns a 64-bit integer, which is what an hrtime_t resolves to anyway.
+
+This implies that this module can only operate with a 64-bit Perl.
+
+=cut
