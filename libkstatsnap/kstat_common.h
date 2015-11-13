ae6debceba13bfe7dc8f4c2d512b6d97d8d35c56
gmarler <gmarler@gmarler.com>
2015-11-12 23:56:44 -0500

    Add CPU snapshot struct

--- libkstatsnap/kstat_common.h
+++ libkstatsnap/kstat_common.h
@@ -6,6 +6,15 @@
 extern "C" {
 #endif
 
+
+#include <stdio.h>
+#include <kstat.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <sys/processor.h>
+#include <sys/pset.h>
+
+
 /* There is no CPU at this CPU location */
 #define ID_NO_CPU -1
 /* This CPU belongs to no pset (number as "pset 0")  */
@@ -25,6 +34,18 @@ enum snapshot_types {
   SNAP_SYSTEM                =   1 << 2;
   /* Interrupt sources and counts */
   SNAP_INTERRUPTS            =   1 << 3;
+};
+
+struct cpu_snapshot {
+  /* If no CPU present, will be ID_NO_CPU */
+  processorid_t cs_id;
+  /* If no pset, will be ID_NO_PSET */
+  psetid_t      cs_pset_id;
+  /* the same state as used in p_online(2) */
+  int           cs_state;
+  /* Statistics for this particular CPU */
+  kstat_t       cs_vm;
+  kstat_t       cs_sys;
 };
 
 
